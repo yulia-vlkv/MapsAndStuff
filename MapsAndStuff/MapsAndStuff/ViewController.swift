01420b6d6356f77d729d943eb1fd46cd81aa0102
Iuliia Vlkv <yu.wlkw@gmail.com>
2022-06-26 07:57:14 +0200

    initial commit

---
+++ MapsAndStuff/MapsAndStuff/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapsAndStuff
+//
+//  Created by Iuliia Volkova on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
